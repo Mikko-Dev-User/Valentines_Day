6d483ce43161ee78507ab26626a9046bb644e7fc
Mikko-Dev-User <mikko.dev.1919@gmail.com>
2026-02-14 08:46:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -222,12 +222,12 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
         height: '0',
         width: '0',
-        videoId: 'aHbzhVOUMDg',
+        videoId: 'Jf-mzP3QhSI',
         playerVars: {
             'autoplay': 1,
             'controls': 0,
             'loop': 1,
-            'playlist': 'aHbzhVOUMDg', // Required for loop to work
+            'playlist': 'Jf-mzP3QhSI', // Required for loop to work
             'showinfo': 0,
             'modestbranding': 1
         },
